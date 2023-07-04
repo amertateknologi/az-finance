523002017b50da96c1c1b1dc3d2c262773ccf643
hifebriansyah <hifebriansyah@gmail.com>
2023-07-05 03:07:49 +0700

    install prompt

--- public/assets/sw.js
+++ public/assets/sw.js
@@ -1,35 +1,33 @@
 // Service Worker registration
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register('/assets/sw.js')
-        .then(registration => {
-          console.log('Service Worker registered:', registration);
-        })
-        .catch(error => {
-          console.log('Service Worker registration failed:', error);
-        });
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("/assets/sw.js")
+            .then((registration) => {
+                console.log("Service Worker registered:", registration);
+            })
+            .catch((error) => {
+                console.log("Service Worker registration failed:", error);
+            });
     });
-  }
-  
-  // Service Worker implementation (service-worker.js file)
-  self.addEventListener('install', event => {
+}
+
+// Service Worker implementation (service-worker.js file)
+self.addEventListener("install", (event) => {
     event.waitUntil(
-      caches.open('my-pwa-cache').then(cache => {
-        return cache.addAll([
-          '/',
-          '/auth'
-          // Add more files to cache as needed
-        ]);
-      })
+        caches.open("my-pwa-cache").then((cache) => {
+            return cache.addAll([
+                "/",
+                "/auth"
+            ]);
+        })
     );
-  });
-  
-  self.addEventListener('fetch', event => {
+});
+
+self.addEventListener("fetch", (event) => {
     event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
     );
-  });
-  
+});
